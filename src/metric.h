6ec262c95c65ebf7d5b4abab69ee864a05d22b9f
Sebastiano Bernuzzi <bernuzzi@caltech.edu>
2015-05-25 22:24:14 -0700

    minor in doc

--- src/metric.h
+++ src/metric.h
@@ -1,140 +1,133 @@
 /**
  * \file metric.h
+ *
  * \brief Function prototypes for EOB metric.
  *
  * \author S.Bernuzzi
  */
 
 /**
- * \fn void eob_met_A_5PNP15_n (double u, void *coefs, 
- *                              double *A, double *dA_u, double *d2A_u)
+ * void eob_met_A_5PNP15_n (double u, void *coefs, double *A, double *dA_u, double *d2A_u)
  *
  * \brief Metric potential \f$A(u)\f$, 5PN expression resummed Pade(1,5).
  *
  * \param[in]  u      1/r coordinate
  * \param[in]  ACoefs PN coefficients
  * \param[out] A      \f$A(u)\f$
- * \param[out] dA     \f$A'(u)\f$
- * \param[out] d2A    \f$A''(u)\f$
+ * \param[out] dA_u   \f$A'(u)\f$
+ * \param[out] d2A_u  \f$A''(u)\f$
  * \return     void
  */
 void eob_met_A_5PNP15_n (double u, void *coefs,
 			 double *A, double *dA_u, double *d2A_u);
 
 /**
- * void eob_met_A_4PNP14_n (double u, void *coefs, 
- *                          double *A, double *dA_u, double *d2A_u)
+ * void eob_met_A_4PNP14_n (double u, void *coefs, double *A, double *dA_u, double *d2A_u)
  *
  * \brief Metric potential \f$A(u)\f$, 4PN expression resummed Pade(1,4).
  *
  * \param[in]  u      1/r coordinate
  * \param[in]  ACoefs PN coefficients
  * \param[out] A      \f$A(u)\f$
- * \param[out] dA     \f$A'(u)\f$
- * \param[out] d2A    \f$A''(u)\f$
+ * \param[out] dA_u   \f$A'(u)\f$
+ * \param[out] d2A_u  \f$A''(u)\f$
  * \return     void
  */
 void eob_met_A_4PNP14_n (double u, void *coefs,
 			 double *A, double *dA_u, double *d2A_u);
 
 /**
- * void eob_met_A_3PNP13_n (double u, void *coefs,
- *   			    double *A, double *dA_u, double *d2A_u)
+ * void eob_met_A_3PNP13_n (double u, void *coefs, double *A, double *dA_u, double *d2A_u)
  *
  * \brief Metric potential \f$A(u)\f$, 3PN expression resummed Pade(1,3).
  *
  * \param[in]  u      1/r coordinate
  * \param[in]  ACoefs PN coefficients
  * \param[out] A      \f$A(u)\f$
- * \param[out] dA     \f$A'(u)\f$
- * \param[out] d2A    \f$A''(u)\f$
+ * \param[out] dA_u   \f$A'(u)\f$
+ * \param[out] d2A_u  \f$A''(u)\f$
  * \return     void
  */
 void eob_met_A_3PNP13_n (double u, void *coefs,
 			 double *A, double *dA_u, double *d2A_u);
 
 /**
- * \fn void eob_met_A_5PN_n (double u, void *coefs,
- *   		             double *A, double *dA_u, double *d2A_u)
+ * void eob_met_A_5PN_n (double u, void *coefs, double *A, double *dA_u, double *d2A_u)
  *
  * \brief Metric potential \f$A(u)\f$, 5PN expression.
  *
  * \param[in]  u      1/r coordinate
  * \param[in]  ACoefs PN coefficients
  * \param[out] A      \f$A(u)\f$
- * \param[out] dA     \f$A'(u)\f$
- * \param[out] d2A    \f$A''(u)\f$
+ * \param[out] dA_u   \f$A'(u)\f$
+ * \param[out] d2A_u  \f$A''(u)\f$
  * \return     void
  */
 void eob_met_A_5PN_n (double u, void *coefs,
 		      double *A, double *dA_u, double *d2A_u);
 
 /**
- * \fn void eob_met_A_4PN_n (double u, void *coefs,
- *   		             double *A, double *dA_u, double *d2A_u)
+ * void eob_met_A_4PN_n (double u, void *coefs, double *A, double *dA_u, double *d2A_u)
  *
  * \brief Metric potential \f$A(u)\f$, 4PN expression.
  *
  * \param[in]  u      1/r coordinate
  * \param[in]  ACoefs PN coefficients
  * \param[out] A      \f$A(u)\f$
- * \param[out] dA     \f$A'(u)\f$
- * \param[out] d2A    \f$A''(u)\f$
+ * \param[out] dA_u   \f$A'(u)\f$
+ * \param[out] d2A_u  \f$A''(u)\f$
  * \return     void
  */
 void eob_met_A_4PN_n (double u, void *coefs,
 		      double *A, double *dA_u, double *d2A_u);
 
 /**
- * \fn void eob_met_A_3PN_n (double u, void *coefs,
- * 		             double *A, double *dA_u, double *d2A_u)
+ * void eob_met_A_3PN_n (double u, void *coefs, double *A, double *dA_u, double *d2A_u)
  *
  * \brief Metric potential \f$A(u)\f$, 3PN expression.
  *
  * \param[in]  u      1/r coordinate
  * \param[in]  ACoefs PN coefficients
  * \param[out] A      \f$A(u)\f$
- * \param[out] dA     \f$A'(u)\f$
- * \param[out] d2A    \f$A''(u)\f$
+ * \param[out] dA_u   \f$A'(u)\f$
+ * \param[out] d2A_u  \f$A''(u)\f$
  * \return     void
  */
 void eob_met_A_3PN_n (double u, void *coefs,
 		      double *A, double *dA_u, double *d2A_u);
 
 /**
- * \fn void eob_met_A_2PN_n (double u, void *coefs,
- *		      double *A, double *dA_u, double *d2A_u)
+ * void eob_met_A_2PN_n (double u, void *coefs, double *A, double *dA_u, double *d2A_u)
  *
  * \brief Metric potential \f$A(u)\f$, 2PN expression.
  *
  * \param[in]  u      1/r coordinate
  * \param[in]  ACoefs PN coefficients
  * \param[out] A      \f$A(u)\f$
- * \param[out] dA     \f$A'(u)\f$
- * \param[out] d2A    \f$A''(u)\f$
+ * \param[out] dA_u   \f$A'(u)\f$
+ * \param[out] d2A_u  \f$A''(u)\f$
  * \return     void
  */
 void eob_met_A_2PN_n (double u, void *coefs,
 		      double *A, double *dA_u, double *d2A_u);
 
 /**
- * \fn void eob_met_A_1PN_n (double u, void *coefs,
- *		      double *A, double *dA_u, double *d2A_u)
+ * void eob_met_A_1PN_n (double u, void *coefs, double *A, double *dA_u, double *d2A_u)
  *
  * \brief Metric potential \f$A(u)\f$, 1PN expression.
  *
  * \param[in]  u      1/r coordinate
  * \param[in]  ACoefs PN coefficients
  * \param[out] A      \f$A(u)\f$
- * \param[out] dA     \f$A'(u)\f$
- * \param[out] d2A    \f$A''(u)\f$
+ * \param[out] dA_u   \f$A'(u)\f$
+ * \param[out] d2A_u  \f$A''(u)\f$
  * \return     void
  */
 void eob_met_A_1PN_n (double u, void *coefs,
 		      double *A, double *dA_u, double *d2A_u);
 
 /**
- * \fn eob_met_dA_rfromu_n (double u, double dA_u)
+ * eob_met_dA_rfromu_n (double u, double dA_u)
  *
  * \brief Return \f$A'(r)\f$ from \f$A'(u)\f$
  *
@@ -145,7 +138,7 @@ void eob_met_A_1PN_n (double u, void *coefs,
 double eob_met_dA_rfromu_n (double u, double dA_u);
 
 /**
- * \fn double eob_met_d2A_rfromu_n (double u, double dA_u, double d2A_u)
+ * double eob_met_d2A_rfromu_n (double u, double dA_u, double d2A_u)
  *
  * \brief Return \f$A''(r)\f$ from \f$A'(u)\f$ and \f$A''(u)\f$
  *
@@ -157,7 +150,7 @@ double eob_met_dA_rfromu_n (double u, double dA_u);
 double eob_met_d2A_rfromu_n (double u, double dA_u, double d2A_u);
 
 /**
- * \fn double *eob_met_ACoefs (double nu)
+ * double *eob_met_ACoefs (double nu)
  *
  * \brief Coefficients of the \f$A(r)\f$ metric potential.
  *
@@ -167,10 +160,10 @@ double eob_met_d2A_rfromu_n (double u, double dA_u, double d2A_u);
 double *eob_met_ACoefs (double nu);
 
 /**
- * \fn void eob_met_B_fromAD_n (double A, double dA, double D, double dD, 
+ * void eob_met_B_fromAD_n (double A, double dA, double D, double dD, 
  *			        double *B, double *dB)
  *
- * \brief  Metric potential \f$B(u)\f$ from \f$A(u)\f$ and \f$D(u)\f$.
+ * \brief Metric potential \f$B(u)\f$ from \f$A(u)\f$ and \f$D(u)\f$.
  *
  * \param[in]  A   \f$A(u)\f$
  * \param[in]  dA  \f$A'(u)\f$
@@ -183,7 +176,7 @@ void eob_met_B_fromAD_n (double A, double dA, double D, double dD,
 			 double *B, double *dB);
 
 /**
- * \fn void eob_met_B_1PN_n (double u, double *BCoefs,
+ * void eob_met_B_1PN_n (double u, double *BCoefs,
  *           		     double *D, double *dD)
  *
  * \brief Metric potential \f$B(u)\f$, 1PN expression.
@@ -198,7 +191,7 @@ void eob_met_B_1PN_n (double u, double *BCoefs,
 		      double *D, double *dD);
 
 /**
- * \fn void eob_met_D_3PNP03_n (double u, double *DCoefs, double *D, double *dD)
+ * void eob_met_D_3PNP03_n (double u, double *DCoefs, double *D, double *dD)
  *
  * \brief Metric potential \f$D(u)\f$, 3PN expression resummed Pade(0,3).
  *
@@ -212,7 +205,7 @@ void eob_met_D_3PNP03_n (double u, double *DCoefs,
 			 double *D, double *dD);
 
 /**
- * \fn void eob_met_D_3PN_n (double u, double *DCoefs, double *D, double *dD)
+ * void eob_met_D_3PN_n (double u, double *DCoefs, double *D, double *dD)
  *
  * \brief Metric potential \f$D(r)\f$, 3PN expression.
  *
@@ -226,7 +219,7 @@ void eob_met_D_3PN_n (double u, double *DCoefs,
 		      double *D, double *dD);
 
 /**
- * \fn void eob_met_D_2PN (double u, double *DCoefs, double *D, double *dD)
+ * void eob_met_D_2PN (double u, double *DCoefs, double *D, double *dD)
  *
  * \brief Metric potential \f$D(u)\f$, 2PN expression.
  *
@@ -240,7 +233,7 @@ void eob_met_D_2PN_n (double u, double *DCoefs,
 		      double *D, double *dD);
 
 /**
- * \fn void eob_met_D_1PN (double u, double *DCoefs, double *D, double *dD)
+ * void eob_met_D_1PN (double u, double *DCoefs, double *D, double *dD)
  *
  * \brief Metric potential \f$D(u)\f$, 1PN expression.
  *
@@ -254,7 +247,7 @@ void eob_met_D_1PN_n (double u, double *DCoefs,
 		      double *D, double *dD);
 
 /**
- * \fn double *eob_met_DCoefs (double nu)
+ * double *eob_met_DCoefs (double nu)
  *
  * \brief Coefficients of the \f$D(r)\f$ metric potential, up ot 3PN.
  *
