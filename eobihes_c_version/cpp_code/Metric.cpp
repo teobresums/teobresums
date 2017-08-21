714166973d5a537cab4d04bff42422c0f4362aac
“Gregorio <gregorio.carullo@ligo.org>
2017-08-21 19:41:33 +0200

    Rewrote expressions in Metric.cpp

--- eobihes_c_version/cpp_code/Metric.cpp
+++ eobihes_c_version/cpp_code/Metric.cpp
@@ -103,13 +103,8 @@ vector<double> Metric(const double r, void *params, bool nnlo_flag)
         double XB  =  1.-XA;
 			   
         /** \bar{lambda} from Yagi et al. REF: */
-        double lambdaAl2;
-        double lambdaAl3;
-        double lambdaAl4;
-
-        double lambdaBl2;
-        double lambdaBl3;
-        double lambdaBl4;
+        double lambdaAl2, lambdaAl3, lambdaAl4;
+        double lambdaBl2, lambdaBl3, lambdaBl4;
 
         lambdaAl2 = (*(input *)params).LambdaAl2;
         lambdaAl3 = (*(input *)params).LambdaAl3;
@@ -120,13 +115,13 @@ vector<double> Metric(const double r, void *params, bool nnlo_flag)
         lambdaBl4 = (*(input *)params).LambdaBl4; 
        
         /** Computing the tidal coupling constants */
-        double kapA2 = 3   * lambdaAl2 * pow(XA, 2.*2 +1.) * q; //Note: kap stands for kappa; see eqn(1) of REF
-        double kapA3 = 15  * lambdaAl3 * pow(XA, 2.*3 +1.) * q;
-        double kapA4 = 105 * lambdaAl4 * pow(XA, 2.*4 +1.) * q;
+        double kapA2 = 3.   * lambdaAl2 * pow(XA, 2.*2 +1.) * q; //Note: kap stands for kappa; see eqn(1) of REF
+        double kapA3 = 15.  * lambdaAl3 * pow(XA, 2.*3 +1.) * q;
+        double kapA4 = 105. * lambdaAl4 * pow(XA, 2.*4 +1.) * q;
  
-        double kapB2 = 3   * lambdaBl2 * pow(XB, 2.*2 +1.) * 1./q;
-        double kapB3 = 15  * lambdaBl3 * pow(XB, 2.*3 +1.) * 1./q;
-        double kapB4 = 105 * lambdaBl4 * pow(XB, 2.*4 +1.) * 1./q;
+        double kapB2 = 3.   * lambdaBl2 * pow(XB, 2.*2 +1.) / q;
+        double kapB3 = 15.  * lambdaBl3 * pow(XB, 2.*3 +1.) / q;
+        double kapB4 = 105. * lambdaBl4 * pow(XB, 2.*4 +1.) / q;
         
         double kapT2 = kapA2 + kapB2;
         double kapT3 = kapA3 + kapB3;
@@ -282,14 +277,14 @@ vector<double> A5pnP15_dd(const double r, void *params)
         lambdaBl4 = (*(input *)params).LambdaBl4;
 
         /** Computing the tidal coupling constants: Eq. (31) of D&N, PRD 81, 084016 (2010) */
-        double kapA2 = 3   * lambdaAl2 * pow(XA, 2.*2 +1.) * q; //Note: kap stands for kappa; see eqn(1) of REF
-        double kapA3 = 15  * lambdaAl3 * pow(XA, 2.*3 +1.) * q;
-        double kapA4 = 105 * lambdaAl4 * pow(XA, 2.*4 +1.) * q;
-
-        double kapB2 = 3   * lambdaBl2 * pow(XB, 2.*2 +1.) * 1./q;
-        double kapB3 = 15  * lambdaBl3 * pow(XB, 2.*3 +1.) * 1./q;
-        double kapB4 = 105 * lambdaBl4 * pow(XB, 2.*4 +1.) * 1./q;						
        
+        double kapA2 = 3.   * lambdaAl2 * pow(XA, 2.*2 +1.) * q; //Note: kap stands for kappa; see eqn(1) of REF
+        double kapA3 = 15.  * lambdaAl3 * pow(XA, 2.*3 +1.) * q;
+        double kapA4 = 105. * lambdaAl4 * pow(XA, 2.*4 +1.) * q;
+
+        double kapB2 = 3.   * lambdaBl2 * pow(XB, 2.*2 +1.) / q;
+        double kapB3 = 15.  * lambdaBl3 * pow(XB, 2.*3 +1.) / q;
+        double kapB4 = 105. * lambdaBl4 * pow(XB, 2.*4 +1.) / q;
  
         double kapT2 = kapA2 + kapB2;
         double kapT3 = kapA3 + kapB3;
