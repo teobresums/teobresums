4c345b27d85e9d4a29e12fe67b115a6ba8b3c2aa
“Gregorio <gregorio.carullo@ligo.org>
2017-08-29 14:27:03 +0200

    Changed grid_lenght to dt+1 in interp_grid.cpp /

--- eobihes_c_version/cpp_code/interp_grid.cpp
+++ eobihes_c_version/cpp_code/interp_grid.cpp
@@ -29,7 +29,7 @@ vector<double> interp_grid(vector<double> t_vec, vector<double> data, double dt)
 {
     int i=0;
     int t_length = t_vec.size();
-    int grid_length = (int)(t_vec.back()-t_vec[0])/dt + 2;
+    int grid_length = (int)(t_vec.back()-t_vec[0])/dt + 1;
 
     double xi, yi;
     vector<double> data_g(grid_length);
