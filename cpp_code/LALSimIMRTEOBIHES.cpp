a78392c28c8b982515d939a1aac2c9d9ebb29d85
Walter Del Pozzo <walter.delpozzo@ligo.org>
2017-08-31 16:23:35 +0200

    removed if of spins for the ringdown

--- cpp_code/LALSimIMRTEOBIHES.cpp
+++ cpp_code/LALSimIMRTEOBIHES.cpp
@@ -381,7 +381,7 @@ void XLALSimIMRTEOBIHES(Waveform **hplus,       /** h+ return array **/
     }
     
     /** Ringdown attachment */
-    if (params.tidal==false && params.spin==false)
+    if (params.tidal==false)
     {
         ringdown(params.nu,params.q,params.dt,final_mass,t_g,MOmg_vecg,hlm_ampl_g,hlm_phase_g);
     }
