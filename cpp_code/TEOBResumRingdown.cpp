3a58b2358c37f51df09f2d22fb12b1f37b92c4da
Walter Del Pozzo <walter.delpozzo@ligo.org>
2017-09-02 12:43:13 +0200

    unified the ringdown files

--- cpp_code/TEOBResumRingdown.cpp
+++ cpp_code/TEOBResumRingdown.cpp
@@ -25,7 +25,9 @@
 
 using namespace::std;
 
+/** Final spin fitting formula */
 /** from https://arxiv.org/abs/1611.00332 */
+
 static double JimenezFortezaRemnantSpin(TEOBResumParams params)
 {
     double nu      = params.nu;
@@ -94,18 +96,19 @@ static double JimenezFortezaRemnantSpin(TEOBResumParams params)
     return X1*X1*chi1+X2*X2*chi2 + Lorb_spin_zero + Lorb_eq_spin + Lorb_uneq_mass;
 }
 
+/** Fits for the 22 mode for spinning systems */
 void QNMHybridFitCab(TEOBResumParams params, vector<double> &a1, vector<double> &a2, vector<double> &a3, vector<double> &a4, vector<double> &b1, vector<double> &b2, vector<double> &b3, vector<double> &b4, vector<gsl_complex> &sigma)
 {
-
+    
     // Shorthands
     int k22 = 1;
     int k21 = 0;
     int k33 = 4;
-
+    
     double nu  = params.nu;
     double nu2 = nu*nu;
     double nu3 = nu2*nu;
-
+    
     for (int i=35; i--; )
     {
         a1[i]=0.;
@@ -117,7 +120,7 @@ void QNMHybridFitCab(TEOBResumParams params, vector<double> &a1, vector<double> 
     b2 = a1;
     b3 = a1;
     b4 = a1;
-
+    
     vector<double> alpha21 = a1;
     vector<double> alpha1  = a1;
     vector<double> omega1  = a1;
@@ -173,7 +176,7 @@ void QNMHybridFitCab(TEOBResumParams params, vector<double> &a1, vector<double> 
         c4phi[k33]   = -7.9494744361321930  * nu  + 2.8106916948840492                          ;
         Domg[k33]    =  2.5797331166178403  * nu2 - 0.5337830158170729 * nu + 0.1930766531716946;
         Amrg[k33]    = -10.4024985230145610 * nu2 + 1.3517710770250695 * nu + 0.4307642913724235;
-
+        
         for (int i=35; i--; ) {
             switch (i) {
                 case 0:
@@ -200,7 +203,7 @@ void QNMHybridFitCab(TEOBResumParams params, vector<double> &a1, vector<double> 
         double omega1_c    = -0.0598837831 * af3 + 0.8082136788 * af2 - 1.7408467418 * af + 1;
         double omega1_d    = -0.2358960279 * af3 + 1.3152369374 * af2 - 2.0764065380 * af + 1;
         omega1[k22] =  0.3736716844 * (omega1_c/omega1_d);
-    
+        
         /** alpha1 is alpha1[k22] */
         double alpha1_c    =  0.1211263886 * af3 + 0.7015835813 * af2 - 1.8226060896 * af + 1;
         double alpha1_d    =  0.0811633377 * af3 + 0.7201166020 * af2 - 1.8002031358 * af + 1;
@@ -272,3 +275,236 @@ void QNMHybridFitCab(TEOBResumParams params, vector<double> &a1, vector<double> 
     }
 }
 
+/** Ringdown waveform template */
+gsl_complex ringdown_match(double x, int k, vector<double> a1, vector<double> a2, vector<double> a3, vector<double> a4, vector<double> b1, vector<double> b2, vector<double> b3, vector<double> b4, vector<gsl_complex> sigma){
+    
+    gsl_complex psi;
+    
+    double amp   =  ( a1[k] * tanh(a2[k]*x +a3[k]) + a4[k] ) ;
+    double phase = -b1[k]*log((1. + b3[k]*exp(-b2[k]*x) + b4[k]*exp(-2.*b2[k]*x))/(1.+b3[k]+b4[k]));
+    
+    psi.dat[0] = amp * exp(-sigma[k].dat[0]*x);
+    psi.dat[1] = - (phase - sigma[k].dat[1]*x); //NOTE: minus sign in front by convention
+    
+    return psi;
+    
+}
+
+/* ringdown calculation and match to the dynamics */ 
+int ringdown(TEOBResumParams params, vector<vector<double> > &t_vec, vector<double> Omega_vec, vector<vector<double> > &hlm_rad, vector<vector<double> > &hlm_phase){
+    
+    //EOBhlm Compute the multipolar resummed waveform.
+    //
+    //   [hlm,phi,omglm,domglm,d2omglm, psilm,Alm,dpsilm] = ...
+    //   EOBhlm(nu,t,phi,r,pph,prstar, Omega,E,Heff, EOBopt, EOBmet)
+    //   return (complex) multipolar wave, phase, frequency, and derivatives,
+    //   and the RWZ normalized (complex) wave, its amplitude and (complex)
+    //   derivative.
+    //
+    //   WAVE = EOBhlm( .... ) return a structure with the wave
+    //
+    
+    //NOTE: before the Omega_vec had a Mbh multiplied onto it!!!!!!!!!!!!!!!!!!!!!!
+    
+    
+    double dt   = params.dt;
+    double nu   = params.nu;
+    double Mbh  = params.Mbh;
+    double chi1 = params.chi1;
+    
+    
+    long int pk_index = Omega_vec.size()-1;
+    double Omega_pk   = Omega_vec[pk_index];
+    long int i        = pk_index-1;
+    while (Omega_vec[i] > Omega_pk) {
+        pk_index = i;
+        Omega_pk = Omega_vec[i];
+        i--;
+    }
+    vector<gsl_complex> Omega_pk_grid(7);
+    Omega_pk_grid[0].dat[0] = t_vec[1][pk_index-3];
+    Omega_pk_grid[0].dat[1] = Omega_vec[pk_index-3];
+    Omega_pk_grid[1].dat[0] = t_vec[1][pk_index-2];
+    Omega_pk_grid[1].dat[1] = Omega_vec[pk_index-2];
+    Omega_pk_grid[2].dat[0] = t_vec[1][pk_index-1];
+    Omega_pk_grid[2].dat[1] = Omega_vec[pk_index-1];
+    Omega_pk_grid[3].dat[0] = t_vec[1][pk_index];
+    Omega_pk_grid[3].dat[1] = Omega_vec[pk_index];
+    Omega_pk_grid[4].dat[0] = t_vec[1][pk_index+1];
+    Omega_pk_grid[4].dat[1] = Omega_vec[pk_index+1];
+    Omega_pk_grid[5].dat[0] = t_vec[1][pk_index+2];
+    Omega_pk_grid[5].dat[1] = Omega_vec[pk_index+2];
+    Omega_pk_grid[6].dat[0] = t_vec[1][pk_index+3];
+    Omega_pk_grid[6].dat[1] = Omega_vec[pk_index+3];
+    
+    double tOmg_pk = 0.;
+    double DeltaT_nqc = 0.;
+    vector<double> tmrg(35);
+    vector<double> tmatch(35);
+    
+    //compute true peak by interpolation from the grid
+    tOmg_pk  = interpolate(dt, Omega_pk_grid);
+    tOmg_pk *= 1./Mbh;
+    
+    //calculate tmatch
+    double xnu =(1.-4.*nu);
+    
+    vector<double> dtmrg(2);
+    int k21 = 0;
+    int k22 = 1;
+    int k33 = 4;
+    
+    
+    /** time-shift needed when the largest object is highly spinning*/
+    if (chi1 >= 0.8498)
+    {
+        /* Interpolating fit for Deltat_NQC. See Eq.(21) of arXiv:1506.08457 */
+        DeltaT_nqc = dtnqc_fit(chi1,0.8498);
+    }
+    else
+    {
+        DeltaT_nqc = 1.;
+    }
+    
+    tmrg[k22] = tOmg_pk-(DeltaT_nqc + 2)/Mbh;     //t_max(A22) => MERGER
+    
+    /** nonspinning case - old */
+    /*tmrg[k22]  = tOmg_pk-3./Mbh; */             // t_max(A22) => MERGER
+    
+    
+    /** only nonspinning case */
+    dtmrg[0]   = 5.70364338 + 1.85804796*xnu  + 4.0332262*xnu*xnu; //k21
+    dtmrg[1]   = 4.29550934 - 0.85938*xnu;                         //k33
+    tmrg[k21]  = tmrg[k22] + dtmrg[0]/Mbh;     // t_max(A21) => peak of 21 mode
+    tmrg[k33]  = tmrg[k22] + dtmrg[1]/Mbh;     // t_max(A33) => peak of 33 mode
+    
+    
+    /* postmerger-ringdown matching time */
+    tmatch = tmrg;
+    
+    int kmax = 35;
+    vector<gsl_complex> sigma(kmax); //move this to another place
+    
+    for (int i=35; i--; ) {
+        sigma[i].dat[0]=0.;
+        sigma[i].dat[1]=0.;
+        tmatch[i] += 2./Mbh;
+    }
+    
+    vector<double> a1(35);
+    vector<double> a2(35);
+    vector<double> a3(35);
+    vector<double> a4(35);
+    vector<double> b1(35);
+    vector<double> b2(35);
+    vector<double> b3(35);
+    vector<double> b4(35);
+    
+    QNMHybridFitCab(params,a1,a2,a3,a4,b1,b2,b3,b4,sigma);
+    
+    /*deleting data points up to tmatch (starting from the back)*/
+    vector<long> I(35);
+    vector<long> Size(35);
+    for (int k = 35; k--; ) {
+        i = t_vec[k].size()-1;
+        Size[k] = t_vec[k].size();
+        switch (k) {
+            case 0:
+                while (t_vec[k][i]/Mbh>tmatch[k]) {
+                    t_vec[k].pop_back();
+                    hlm_rad[k].pop_back();
+                    hlm_phase[k].pop_back();
+                    i--;
+                }
+                i++;
+                break;
+            case 1:
+                while (t_vec[k][i]/Mbh>tmatch[k]) {
+                    t_vec[k].pop_back();
+                    hlm_rad[k].pop_back();
+                    hlm_phase[k].pop_back();
+                    i--;
+                }
+                i++;
+                break;
+            case 4:
+                while (t_vec[k][i]/Mbh>tmatch[k]) {
+                    t_vec[k].pop_back();
+                    hlm_rad[k].pop_back();
+                    hlm_phase[k].pop_back();
+                    i--;
+                }
+                i++;
+                break;
+            default:
+                break;
+        }
+        I[k] = i;
+    }
+    
+    //Calculate deltaphi
+    vector<gsl_complex> psi(35);
+    vector<double> Deltaphi(35);
+    for (int k=35; k--; ) {
+        double x    = t_vec[k][I[k]]/Mbh-tmrg[k];
+        psi[k]      = ringdown_match(x, k, a1, a2, a3, a4, b1, b2, b3, b4, sigma);
+        Deltaphi[k] = psi[k].dat[1] - hlm_phase[k][I[k]];
+    }
+    
+    /** add 200 points of ringdown attachment */
+    /** if we select the number of points in each mode to be such that the number of output points
+     is the same, we do not need any interpolation, since the vectors are defined on the same time grid. */
+    int Nringdown = 500;
+    for (int k=35; k--; ) {
+        double t = t_vec[k][I[k]];
+        int n_removed = Size[k]-I[k];
+        switch (k) {
+            case 0:
+                for (int j=0; j < Nringdown+n_removed; j++) {
+                    double x = t/Mbh-tmrg[k];
+                    psi[k] = ringdown_match(x, k, a1, a2, a3, a4, b1, b2, b3, b4, sigma);
+                    
+                    psi[k].dat[1]  = psi[k].dat[1] - Deltaphi[k];
+                    hlm_rad[k].push_back(psi[k].dat[0]);
+                    hlm_phase[k].push_back(psi[k].dat[1]);
+                    t_vec[k].push_back(t);
+                    t += dt;
+                }
+                break;
+            case 1:
+                for (int j=0; j < Nringdown+n_removed; j++) {
+                    double x = t/Mbh-tmrg[k];
+                    psi[k] = ringdown_match(x, k, a1, a2, a3, a4, b1, b2, b3, b4, sigma);
+                    
+                    psi[k].dat[1]  = psi[k].dat[1] - Deltaphi[k];
+                    hlm_rad[k].push_back(psi[k].dat[0]);
+                    hlm_phase[k].push_back(psi[k].dat[1]);
+                    t_vec[k].push_back(t);
+                    t += dt;
+                }
+                break;
+            case 4:
+                for (int j=0; j < Nringdown+n_removed; j++) {
+                    double x = t/Mbh-tmrg[k];
+                    psi[k] = ringdown_match(x, k, a1, a2, a3, a4, b1, b2, b3, b4, sigma);
+                    
+                    psi[k].dat[1]  = psi[k].dat[1] - Deltaphi[k];
+                    hlm_rad[k].push_back(psi[k].dat[0]);
+                    hlm_phase[k].push_back(psi[k].dat[1]);
+                    t_vec[k].push_back(t);
+                    t += dt;
+                }
+                break;
+            default:
+                for (int j=0; j < Nringdown+n_removed-1; j++) {
+                    hlm_rad[k].push_back(0.0);
+                    hlm_phase[k].push_back(0.0);
+                    t_vec[k].push_back(t);
+                    t += dt;
+                }
+                break;
+        }
+    }
+    
+    return 0;
+}
