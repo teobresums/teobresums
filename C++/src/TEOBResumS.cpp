51f981be45be0f4210c0e4c793b6f97518adcf04
“Gregorio <gregorio.carullo@ligo.org>
2017-09-04 11:03:24 +0200

    Deleted print statement

--- C++/src/TEOBResumS.cpp
+++ C++/src/TEOBResumS.cpp
@@ -141,13 +141,11 @@ void TEOBResumS(Waveform **hplus,       /** h+ return array **/
     
     if (params.flags.spin==1)
     {
-        cout << "Spinning:" << params.flags.spin << endl;
         sys          = {s_RHS, NULL , 4, &params};
         initial_data = s_initial(&params);
     }
     else
     {
-        cout << "Nonspinning:" << params.flags.spin << endl;
         sys          = {rhs, NULL , 4, &params};
         initial_data = initial(&params);
     }
