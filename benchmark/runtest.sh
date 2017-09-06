ad0bd71bb484a76115a7f14b65db4f89c0d31683
bernuzzi <sebastiano.bernuzzi@unipr.it>
2017-09-06 22:24:01 +0200

    a simple benchmark system based on parfiles

--- benchmark/runtest.sh
+++ benchmark/runtest.sh
@@ -1,39 +1,12 @@
 #!/bin/bash
-# Simple script to benchmark the code
+# Script to run all the parfiles
 
-SRCPATH=/home/bernuzzi/Codes/teobresums/C++/
-MAKEFILE=/home/bernuzzi/Codes/teobresums/C++/Makefile.TEOBResumS
-
-#
-#
-#
-NOW=$(date +"%Y%m%d%H%M%S")
-TOP=$(pwd)
-GIT=$(git rev-list HEAD | wc -l)
-NAME="bench_${GIT}_${NOW}"
-
-#
-#
-#
-mkdir -pv $NAME
-cd $NAME
-WORKDIR=$(pwd)
-echo $(pwd)
-cp -r $SRCPATH/src .
-cp $MAKEFILE Makefile
-make 
-tar zcvf src.tgz Makefile src/
-rm -rvf src/ obj/ Makefile
-git log --format="%H" -n 1 > git_status.txt
-
-#
-#
-#
-for p in `ls ../parfiles/*.par`; do
-    test=$(basename "$p")
-    test="${test%.*}"
-    echo "$test"
-    ./TEOBResumS -p $p > $test.out
-    mkdir -p data/$test/
-    mv waveform.dat data/$test/
+for p in `ls ./parfiles/*.par`; do
+    name=$(basename "$p")
+    name="${name%.*}"
+    echo "$name"
+    ./TEOBResumS.x -p $p > $name.out
+    mkdir -p data/$name/
+    mv waveform.dat data/$name/
 done
+
