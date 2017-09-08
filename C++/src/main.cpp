bf562ac745f55f11c73da04a63bf21d4c3116388
Ka Wa Tsang <kwtsang@nikhef.nl>
2017-09-08 18:37:07 +0200

    indentation

--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -102,7 +102,9 @@ int main (int argc, char* argv[])
         fprintf(stderr,USAGE);
         fprintf(stderr,"\nOPTIONS:\n");
         for (int i = 0; i < (Nopt*4); i=i+4)
+        {
             fprintf(stderr,"\t%-20s %-10s %s [%s]\n",optstr[i],optstr[i+1],optstr[i+2],optstr[i+3]);
+        }
         exit(0);
     }
    
@@ -111,9 +113,11 @@ int main (int argc, char* argv[])
         if ((strcmp(argv[i],"-h")==0)||(strcmp(argv[i],"-help")==0))
         {
             fprintf(stderr,USAGE);
-	    fprintf(stderr,"\nOPTIONS:\n");
-	    for (int i = 0; i < (Nopt*4); i=i+4)
-	      fprintf(stderr,"\t%-20s %-10s %s [%s]\n",optstr[i],optstr[i+1],optstr[i+2],optstr[i+3]);
+            fprintf(stderr,"\nOPTIONS:\n");
+            for (int i = 0; i < (Nopt*4); i=i+4)
+            {
+              fprintf(stderr,"\t%-20s %-10s %s [%s]\n",optstr[i],optstr[i+1],optstr[i+2],optstr[i+3]);
+            }
             exit(0);
         }
         else if (strcmp(argv[i],"-p")==0)
@@ -122,16 +126,16 @@ int main (int argc, char* argv[])
             printf("Reading parfile: %s\n",parfile);
 
             TEOBResumParams params = read_config(parfile);
-            q = params.q;
-            m1 = params.mtot * q/(1.+q);
-            m2 = params.mtot - m1;
-            chi1 = params.chi1;
-            chi2 = params.chi2;
-            dt = params.dt;
-	    f_min = params.f_min;
-            lm = params.lm;
-            LambdaAl2 = params.LambdaAl2;
-            LambdaBl2 = params.LambdaBl2;
+            q               = params.q;
+            m1              = params.mtot * q/(1.+q);
+            m2              = params.mtot - m1;
+            chi1            = params.chi1;
+            chi2            = params.chi2;
+            dt              = params.dt;
+            f_min           = params.f_min;
+            lm              = params.lm;
+            LambdaAl2       = params.LambdaAl2;
+            LambdaBl2       = params.LambdaBl2;
             flags.Yagi_fits = params.flags.Yagi_fits;
             if (flags.Yagi_fits==0)
             {
@@ -140,17 +144,17 @@ int main (int argc, char* argv[])
                 LambdaAl4 = params.LambdaAl4;
                 LambdaBl4 = params.LambdaBl4;
             }
-	    distance = params.distance;
-	    inclination = params.iota;
-	    polarisation = params.psi;
-            flags.RWZ  = params.flags.RWZ;
-            flags.solver_scheme = params.flags.solver_scheme;
-            flags.tidal = params.flags.tidal;
-            flags.speedy = params.flags.speedy;
-            flags.multipoles = params.flags.multipoles;
+            distance              = params.distance;
+            inclination           = params.iota;
+            polarisation          = params.psi;
+            flags.RWZ             = params.flags.RWZ;
+            flags.solver_scheme   = params.flags.solver_scheme;
+            flags.tidal           = params.flags.tidal;
+            flags.speedy          = params.flags.speedy;
+            flags.multipoles      = params.flags.multipoles;
             flags.geometric_units = params.flags.geometric_units;
 
-	    // flags->spin = params.flags.spin; FIX ME, NOW IT WORKS A CDC
+     // flags->spin = params.flags.spin; FIX ME, NOW IT WORKS A CDC
 
             break;
         }
@@ -308,28 +312,28 @@ int main (int argc, char* argv[])
         }
 
         TEOBResumS(&hplus,
-                    &hcross,
-                    m1,
-                    m2,
-                    0.0,
-                    0.0,
-                    chi1,
-                    0.0,
-                    0.0,
-                    chi2,
-                    inclination,
-                    polarisation,
-                    f_min,
-                    dt,
-                    LambdaAl2,
-                    LambdaBl2,
-                    LambdaAl3,
-                    LambdaBl3,
-                    LambdaAl4,
-                    LambdaBl4,
-                    distance,
-                    lm,
-                    &flags);
+                   &hcross,
+                   m1,
+                   m2,
+                   0.0,
+                   0.0,
+                   chi1,
+                   0.0,
+                   0.0,
+                   chi2,
+                   inclination,
+                   polarisation,
+                   f_min,
+                   dt,
+                   LambdaAl2,
+                   LambdaBl2,
+                   LambdaAl3,
+                   LambdaBl3,
+                   LambdaAl4,
+                   LambdaBl4,
+                   distance,
+                   lm,
+                   &flags);
         
         std::FILE* f = std::fopen(output, "w");
         int i        = 0;
