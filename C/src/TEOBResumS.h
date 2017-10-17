55ab28a5ddd39ab104e21aa5f01013a2d624f765
Ka Wa Tsang <kwtsang@nikhef.nl>
2017-10-17 14:53:45 +0200

    Fixed typos and included a standard library for boolean value.

--- C/src/TEOBResumS.h
+++ C/src/TEOBResumS.h
@@ -26,8 +26,9 @@
  *
  */
 
-#include <stlib.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <libconfig.h> /* library to manage parameters */
 #include <math.h>
 
@@ -335,4 +336,4 @@ void eob_wav_flm_s(double x, double nu, double X1, double X2, double chi1, doubl
 void eob_wav_hlmNQC_find_a1a2a3(int size, double *T, double *r, double *w, double *pph, double *pr_star, double *Omg_orb, double *ddotr, Waveform_lm *h, Dynamics *dyn, Waveform_lm *hnqc);
 void eob_wav_hlmNQC(double  nu, double  r, double  prstar, double  Omega, double  ddotr, Waveform_lm_t *psilmnqc);
 void eob_wav_ringdown_template(double x, double a1, double a2, double a3, double a4, double b1, double b2, double b3, double b4, double *sigma, double *psi);
-void eob_wav_ringdown(doulble *t, double *Omega, Dynamics *dyn, Waveform_lm *hlm);
+void eob_wav_ringdown(double *t, double *Omega, Dynamics *dyn, Waveform_lm *hlm);
