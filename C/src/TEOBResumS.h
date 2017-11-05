1d32e8ed49cb986bde60e1fd5531b50ba39ab5d3
bernuzzi <sebastiano.bernuzzi@unipr.it>
2017-11-05 22:19:29 +0100

    debug of parameters routines done. now using hack to merger par db from 2 parfiles, can be improved.

--- C/src/TEOBResumS.h
+++ C/src/TEOBResumS.h
@@ -242,13 +242,16 @@ typedef struct tagDynamics
 void par_db_init ();
 void par_db_free ();
 void par_db_default ();
-void par_file_parse (char *fname, int merge);
+void par_file_parse (char *fname);
+void par_file_parse_merge (char *fname);
 void par_db_write_file (char *fname);
 void par_db_screen ();
 void par_set_i(const char *key, int val);
 void par_set_b(const char *key, int val);
 void par_set_d(const char *key, double val);
 void par_set_s(const char *key, const char *val);
+void par_set_arrayi (const char *key, int *array, int n);
+void par_set_arrayd (const char *key, double *array, int n);
 int par_get_i(const char *key);
 int par_get_b(const char *key);
 double par_get_d(const char *key);
--- C/src/TEOBResumSPars.c
+++ C/src/TEOBResumSPars.c
@@ -30,7 +30,7 @@
 
 #include "TEOBResumS.h"
 
-#define DEBUG_THIS_FILE 0 /* to compile and debug this files */
+#define DEBUG_THIS_FILE 1 /* to compile and debug this files */
 #if (DEBUG_THIS_FILE)
 #undef errorexit
 #undef errorexits
@@ -54,46 +54,61 @@ void par_db_free ()
   config_destroy(cf);
 }
 
-void par_file_parse (char *fname, int merge)
-{
-  if (merge) {
-    
-    /*
-    config_t cfg1, *cf1;
-    cf1 = &cfg1;
-    config_init(cf1);
-    config_setting_t * csroot1 = config_root_setting(cf1);
-    config_setting_t * cs1;
-    int np = config_setting_length(csroot1);
-    printf("%d\n",np);
-    int i, type;
-    char key[STRLEN];
-    for (i = 0; i < np; i++) {
-      cs1  = config_setting_get_elem(csroot1, i);
-      strcpy(key, config_setting_name(cs1));
-      type = config_setting_type(cs1);
-      printf("%d %s\n",type,key);
-      if      (type==CONFIG_TYPE_INT)   par_set_i(key, config_setting_get_int(cs1));
-      else if (type==CONFIG_TYPE_BOOL)  par_set_b(key, config_setting_get_bool(cs1));
-      else if (type==CONFIG_TYPE_FLOAT) par_set_d(key, config_setting_get_float(cs1));
-      else if (type==CONFIG_TYPE_ARRAY) {
-      } else errorexit("unkown parameter type");
-      }  
-    config_destroy(cf1);
-    */
-
-  } else {
-    
-    if (!(config_read_file(cf, fname))) {
-      fprintf(stderr, "%s = %d - %s\n",
-	      config_error_file(cf),
-	      config_error_line(cf),
-	      config_error_text(cf));
-      config_destroy(cf);
-      errorexit("Problem reading file.");
-    }
-    
+void par_file_parse (char *fname)
+{    
+  if (!(config_read_file(cf, fname))) {
+    fprintf(stderr, "%s = %d - %s\n",
+	    config_error_file(cf),
+	    config_error_line(cf),
+	    config_error_text(cf));
+    config_destroy(cf);
+    errorexit("Problem reading file.");
   }
+}
+
+void par_file_parse_merge (char *fname)
+{
+  /* parse the file in a new cfg */
+  config_t cfg1, *cf1;
+  cf1 = &cfg1;
+  config_init(cf1);
+  if (!(config_read_file(cf1, fname))) {
+    fprintf(stderr, "%s = %d - %s\n",
+	    config_error_file(cf1),
+	    config_error_line(cf1),
+	    config_error_text(cf1));
+    config_destroy(cf1);
+    errorexit("Problem reading file.");
+  }
+  /* set parameter values in db */
+  config_setting_t * csroot1 = config_root_setting(cf1);
+  config_setting_t * cs1, * array;
+  int np = config_setting_length(csroot1);
+int i, type, n;
+  char key[STRLEN];
+  for (i = 0; i < np; i++) {
+    cs1  = config_setting_get_elem(csroot1, i);
+    strcpy(key, config_setting_name(cs1));
+    type = config_setting_type(cs1);
+    if      (type==CONFIG_TYPE_INT)   par_set_i(key, config_setting_get_int(cs1));
+    else if (type==CONFIG_TYPE_BOOL)  par_set_b(key, config_setting_get_bool(cs1));
+    else if (type==CONFIG_TYPE_FLOAT) par_set_d(key, config_setting_get_float(cs1));
+    else if (type==CONFIG_TYPE_ARRAY) {
+      /* this is a hack, only for int type */
+      if (cs1 != NULL) {
+	n = config_setting_length(cs1);
+	int * array = (int *) malloc (n * sizeof(int));
+	if (!array) errorexit("out of memory");
+	for (i = 0; i < n; i++) {
+	  array[i] = config_setting_get_int_elem(cs1, i);
+	}
+	par_set_arrayi (key, array, n);
+	free(array);
+      } 
+    } else errorexit("unkown parameter type");
+  }
+  /* free extra cfg */
+  config_destroy(cf1);
 }
 
 /** default values for parameters are expected in file
@@ -109,7 +124,7 @@ void par_db_default ()
     return;
   }
   else {
-    par_file_parse (strcat(eobcodepath,"/par/default.par"),0);
+    par_file_parse (strcat(eobcodepath,"/par/default.par"));
   }
 }
 
@@ -127,7 +142,7 @@ void par_db_screen ()
   config_write(cf, stderr);
 }
 
-/* 'get' routines, assume all settings are root */
+/* 'get' routines */
 
 int par_get_i (const char *key)
 {
@@ -229,27 +244,39 @@ void par_set_s (const char *key, const char *val)
     errorexits("unknown parameter/wrong type for",key);
 }
 
-/*
 void par_set_arrayi (const char *key, int *array, int n)
 {
-  const config_setting_t *cs = config_lookup(cf, key);
-  if (cs != NULL) {
-    int l = config_setting_length(cs);
-    array = (int *) realloc(array, n);
-
-(int *) malloc (l * sizeof(int));
-    if (!array) errorexit("out of memory");
-    int i;
-    for (i = 0; i < l; i++) {
-      array[i] = config_setting_get_int_elem(cs, i);
-    }
-    *n = l;
-    return array;
-  } else
-    errorexits("unknown parameter",key);
+  config_setting_t *cs = config_lookup(cf, key);
+  config_setting_t *parent = config_setting_parent(cs);
+  int i;
+  /* remove what is there */
+  if (!config_setting_remove(parent, key))
+    errorexits("error removing array",key);
+  /* add the new */
+  config_setting_t *sarray, *a;
+  sarray = config_setting_add(parent, key, CONFIG_TYPE_ARRAY);
+  for(i = 0; i < n; ++i) {
+    a = config_setting_add(sarray, NULL, CONFIG_TYPE_INT);
+    config_setting_set_int(a, array[i]);
+  }
 }
-*/
 
+void par_set_arrayd (const char *key, double *array, int n)
+{
+  config_setting_t *cs = config_lookup(cf, key);
+  config_setting_t *parent = config_setting_parent(cs);
+  int i;
+  /* remove what is there */
+  if (!config_setting_remove(parent, key))
+    errorexits("error removing array",key);
+  /* add the new */
+  config_setting_t *sarray, *a;
+  sarray = config_setting_add(parent, key, CONFIG_TYPE_ARRAY);
+  for(i = 0; i < n; ++i) {
+    a = config_setting_add(sarray, NULL, CONFIG_TYPE_FLOAT);
+    config_setting_set_int(a, array[i]);
+  }
+}
 
 #if (!DEBUG_THIS_FILE)
 
@@ -263,7 +290,7 @@ void TEOBResumSSetParameters(char *s, int pr)
 
   //if (mode==INPUT_FILE) {
   /* Parse input parfile */
-  par_file_parse (s);
+  par_file_parse_merge (s);
   //} 
   //if (mode==COMMAND_LINE) {
   /* Parse command line */
@@ -422,9 +449,11 @@ void TEOBResumSSetParameters(char *s, int pr)
 
 #else
 
-/* test */
+/* test 
+   gcc TEOBResumSPars.c -lconfig -o test.x */
 int main (int argc, char* argv[])
 {
+  int idx,n;
   /* init db */
   par_db_init ();
   /* parse default params from file */
@@ -434,7 +463,6 @@ int main (int argc, char* argv[])
   /* test 'get' */
   printf("get-test: size = %d\n",par_get_i("size"));
   printf("get-test: M = %e\n",par_get_d("M"));
-  int idx,n;
   int *i = par_get_arrayi ("output_lm", &n);
   printf("get-test: array_length n=%d\n",n);
   for (idx=0; idx<n; idx++) printf("get-test: array[%d]=%d\n",idx,i[idx]);
@@ -443,16 +471,22 @@ int main (int argc, char* argv[])
   par_set_d("M",100);
   printf("set-test: size = %d (=10)\n",par_get_i("size"));
   printf("set-test: M = %e (=100)\n",par_get_d("M"));
+  n=6;
+  int j[] = {1,2,3,4,5,6};
+  par_set_arrayi ("output_lm", j, n);
+  int *jj = par_get_arrayi ("output_lm", &n);
+  printf("get-test: array_length n=%d\n",n);
+  for (idx=0; idx<n; idx++) printf("get-test: array[%d]=%d\n",idx,jj[idx]);
   /* parse other params */
-  //par_file_parse ("../par/test.par", 1);
+  par_file_parse_merge ("../par/test.par");
   /* more operations */
-  //printf("get-test: size = %d\n",par_get_i("size"));
-  //par_set_i("size",20);
-  //printf("set-test: size = %d (=20)\n",par_get_i("size"));
+  printf("get-test: size = %d\n",par_get_i("size"));
+  par_set_i("size",20);
+  printf("set-test: size = %d (=20)\n",par_get_i("size"));
   /* display new db */
-  //par_db_screen ();  
+  par_db_screen ();  
   /* write out db */
-  //par_db_write_file("../par/test1.par");
+  par_db_write_file("../par/test1.par");
   /* free db */
   par_db_free ();
   return OK;
