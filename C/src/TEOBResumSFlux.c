46529ccb3a8ef31b84a32e0624479a2cfd857ba7
bernuzzi <sebastiano.bernuzzi@gmail.com>
2018-04-26 10:37:28 +0200

    fixing bug found by Sarp. BUG2: wrong factor X12 for tidal nonspinning Newtonian flux in 21,31,33 modes

--- C/src/TEOBResumSFlux.c
+++ C/src/TEOBResumSFlux.c
@@ -257,9 +257,9 @@ double eob_flx_Flux_s(double x, double Omega, double r_omega, double E, double H
       /* Correct (2,1), (3,1) and (3,3) ( sp2 = 1 ) */
       //SARP: the following is not needed I think, can you check?
       double x6 = gsl_pow_int(x, 6);
-      FNewtlm[0] = CNlm[0] * x6 * X12; /* (2,1) */
-      FNewtlm[2] = CNlm[2] * x6 * X12; /* (3,1) */
-      FNewtlm[4] = CNlm[4] * x6 * X12; /* (3,3) */
+      FNewtlm[0] = CNlm[0] * x6; /* (2,1) */
+      FNewtlm[2] = CNlm[2] * x6; /* (3,1) */
+      FNewtlm[4] = CNlm[4] * x6; /* (3,3) */
     }
   }
 
