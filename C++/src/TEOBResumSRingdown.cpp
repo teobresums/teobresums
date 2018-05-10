7f6c004dd172c4a2434c95bfaf17d1ca91c8bc49
alex_nagar <alex_nagar@bitbucket.org>
2018-05-10 18:16:42 +0200

    BIG FIX: new fit for c3 and simplified treatment of DeltaT_nqc, that now does not depend anymore on chi, no special treatment is necessary. Cross check with the matlab still to be done. But the waveform look clean and sane

--- C++/src/TEOBResumSRingdown.cpp
+++ C++/src/TEOBResumSRingdown.cpp
@@ -555,12 +555,32 @@ int ringdown(TEOBResumParams params, vector<vector<double> > &t_vec, vector<doub
     int k22 = 1;
     int k33 = 4;
     
-    
+    /* Additional time-shift only needed ONLY for large, negative, spins.
+       This change from 1.0 to 4.0 eliminates unphysical features in the
+       frequency related to the imperfect behavior of the NQC functions */
+
+    if ((chi1 <-0.85) && (nu <= 14./225.))
+      {
+	DeltaT_nqc = 4.0;
+      }
+    else
+    {
+        DeltaT_nqc = 1.0;
+    }
+
+    /****************************************************************************
+     All this commented block belongs to pre May 10, 2018. It was the hack needed
+     from the very beginning, 2013, to account for high-positive spin. This was
+     actually related to a factor X1-X2 missing in the l=5, m=odd modes. Fixed
+     this, all this is now useless, though it needs a new representation of c3
+     - THIS COMMENTED BLOCK TO BE DELETED
+     ****************************************************************************
+    //===========================================================================
     /* Additional time-shift needed when the largest object  is  highly spinning
        and/or for high, anti-aligned spins. Quick hack that will be removed once
        the iResum waveform will be available */
-    if (chi1 >= 0.8498)
-    {
+    //if (chi1 >= 0.8498)
+    //{
        
       /* Interpolating fit for Deltat_NQC. See Eq.(21) of arXiv:1506.08457 
 	 This is a formula that was obtained in the equal-mass, equal-spin
@@ -568,23 +588,24 @@ int ringdown(TEOBResumParams params, vector<vector<double> > &t_vec, vector<doub
 	 BH is larger than 0.8498. This is a guess to extrapolate the model
 	 outside the domain of calibration */
       
-        DeltaT_nqc = dtnqc_fit(chi1,0.8498);
-    }
-    else if ((chi1 <=-0.70) && (nu <= 8./81.))
+    //    DeltaT_nqc = dtnqc_fit(chi1,0.8498);
+    //}
+    //else if ((chi1 <=-0.70) && (nu <= 8./81.))
       /* This condition was a simple hack to avoid unphysical features in the
          modulus amplitude when one (or both) the spins are large and negative
          and the mass ratio is large. This little modification in the location
          of the NQC point guarantees that the determination of the NQC parameters
          guarantees just a small perturbation of the non-NQC EOB waveform. The
          iResum waveform will be robust enough that this hack will not be needed*/  
-      {
-	DeltaT_nqc = 4.0;
-      }    
-    else
+    // {
+    //DeltaT_nqc = 4.0;
+    //  }    
+    //else
     /* standard choice inspired by test-particle results */  
-    {
-        DeltaT_nqc = 1.;
-    }
+    //{
+    //    DeltaT_nqc = 1.;
+    //}
+    //=============================================================================
     
     tmrg[k22] = tOmg_pk-(DeltaT_nqc + 2)/Mbh;     //t_max(A22) => MERGER
     
