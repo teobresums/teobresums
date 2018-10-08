7bab9d2d593304a76ef9aec22b31db5b462cf445
akcays <2mad2max@gmail.com>
2018-10-08 16:13:40 +0200

    Hopeful final resolution of conflict. Will test code next

--- C/src/TEOBResumSMetric.c
+++ C/src/TEOBResumSMetric.c
@@ -449,23 +449,8 @@ void eob_metric(double r, Dynamics *dyn, double *A, double *B, double *dA, doubl
   const double dD  = 6.*u2*(2.*nu*u-(3.*nu-26.)*nu*u2)*D*D;
 
   /* B potential and derivative with respect to r */
-  double Btmp, dBtmp_r;
   Btmp   = D/(Atmp);
   dBtmp_r  = (dD*(Atmp) - D*(*dA))/((Atmp)*(Atmp));
-
-  /* Add here tides if needed */
-  if (dyn->use_tidal) {
-    double BT, dBT;
-    // Vines, Flanagan term:
-    double kT2 = par_get_d("kappaTl2");
-    BT = kT2*3.*(3. - 5.*nu)*u6;
-    dBT = -kT2*18.*(3. - 5.*nu)*u4*u3;  
-    Btmp  += BT;
-    dBtmp_r += dBT;
-  }
-
-  Btmp    += D/(Atmp);
-  dBtmp_r += (dD*(Atmp) - D*(*dA))/((Atmp)*(Atmp));
 
   *B  = Btmp;
   *dB = dBtmp_r;
