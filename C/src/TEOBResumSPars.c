61da00a6fb83ca38f6c91d34e150cc7be7647d84
sebastiano bernuzzi <sebastiano.bernuzzi@gmail.com>
2018-10-29 19:10:24 +0100

    fixed two bug from previous commit

--- C/src/TEOBResumSPars.c
+++ C/src/TEOBResumSPars.c
@@ -530,7 +530,9 @@ void eob_set_params(char *s, int n)
     if (par_get_i("interp_uniform_grid")) {
       /* Output will be interpolated on uniform grid 
 	 Set dt = 0.5M by default */
-      par_set_d("dt", 0.5*M );
+      dt = 0.5;
+      par_set_d("dt", dt);
+      par_set_d("dt_interp", dt);
     }
     if (VERBOSE) PRFORMd("dt",dt);
   }
