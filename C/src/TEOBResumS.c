8b5125208e7614653937fad2ab359269ce2b1a48
akcays <2mad2max@gmail.com>
2018-10-31 16:03:57 +0100

    BUG fix in main.c. Old version gave size_interp in the millions when using physical units.

--- C/src/TEOBResumS.c
+++ C/src/TEOBResumS.c
@@ -622,12 +622,12 @@ int main (int argc, char* argv[])
   /** Computation of (h+,hx) */
   compute_hpc(hlm, nu, M, distance, amplitude_prefactor, psi, iota, hpc);
 
-#if (0)
+#if (1)
   if (par_get_i("interp_uniform_grid")) { 
     
     /** Interp to uniform grid (if needed) */
     const double dt_interp = par_get_d("dt_interp");
-    int size_interp = get_uniform_size(hpc->time[hlm->size-1], hlm->time[0], dt_interp);
+    int size_interp = get_uniform_size(hlm->time[size-1], hlm->time[0], dt_interp); 
     Waveform_interp (hpc, size_interp, hpc->time[0], dt_interp * M, "hpc_interp");
     if (par_get_i("output_multipoles")) 
       Waveform_lm_interp (hlm, size_interp, hlm->time[0], dt_interp, "hlm_interp");
