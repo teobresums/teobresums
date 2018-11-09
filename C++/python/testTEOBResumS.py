9544f71935e12b64c72dc049192e797c829c3cc9
Walter Del Pozzo <walter.delpozzo@ligo.org>
2018-11-09 11:48:34 +0100

    attempt at filtering out non sorted grid points for the ringdown attachment

--- C++/python/testTEOBResumS.py
+++ C++/python/testTEOBResumS.py
@@ -2,19 +2,20 @@ from pyTEOBResumS import pyTEOBResumS
 import numpy as np
 #import lalsimulation as lalsim
 
-m1=40.0
-m2=40.0
+m1=61.27
+m2=59.77
 spin1x = 0.0
 spin1y = 0.0
-spin1z = 0.0
+spin1z = -0.58
 spin2x = 0.0
 spin2y = 0.0
-spin2z = 0.0
-inclination = 0.0
-phase = 0.0
-f_min = 20.0
+spin2z = -0.57
+inclination = 1.6604149
+phase = 0.6868080
+f_min = 10.0
 sampling_rate = 2048.
 dt = 1./sampling_rate
+
 LambdaAl2 = 0.0
 LambdaBl2 = 0.0
 LambdaAl3 = 0.0
@@ -23,15 +24,15 @@ LambdaAl4 = 0.0
 LambdaBl4 = 0.0
 distance = 500.0
 
-flags ={'NQC':0,
+flags ={'NQC':1,
         'tidal':0,
         'speedy':1,
         'dynamics':0,
         'solver_scheme':0,
         'RWZ':0,
         'Yagi_fits':0,
-        'spin':0,
-        'multipoles':0,
+        'spin':1,
+        'multipoles':-1,
         'geometric_units':0,
         'set':0
         }
@@ -49,7 +50,7 @@ fig = plt.figure()
 ax1 = fig.add_subplot(211)
 ax2 = fig.add_subplot(212)
 
-for inclination in [0.0]:#,np.pi/3.,np.pi/2.]:
+for f_min in [5,10,20]:#,np.pi/3.,np.pi/2.]:
     h = pyTEOBResumS(m1,
                     m2,
                     spin1x,
@@ -64,11 +65,11 @@ for inclination in [0.0]:#,np.pi/3.,np.pi/2.]:
                     dt,
                     LambdaAl2,
                     LambdaBl2,
-                     LambdaAl3,
-                     LambdaBl3,
-                     LambdaAl4,
-                     LambdaBl4,
-                     distance,
+                    LambdaAl3,
+                    LambdaBl3,
+                    LambdaAl4,
+                    LambdaBl4,
+                    distance,
                     lm,
                     flags)
                     
@@ -85,10 +86,10 @@ for inclination in [0.0]:#,np.pi/3.,np.pi/2.]:
 #                                 amp_order, phase_order,
 #                                 approx)
 
-
-    ax1.plot(h[:,0],label=str(inclination))
+    t = dt*np.arange(h.shape[0])
+    ax1.plot(t, h[:,0],label=str(f_min))
 #    ax1.plot(hp.data.data,linestyle='dashed')
-    ax2.plot(h[:,1])
+    ax2.plot(t, h[:,1])
 #    ax2.plot(hc.data.data,linestyle='dashed')
 ax1.legend()
 plt.show()
