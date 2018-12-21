bf72b13250581f15ab0b228a9559f96ab3c00c68
akcays <2mad2max@gmail.com>
2018-12-21 18:19:35 +0100

    BUG fix

--- C/src/TEOBResumSUtils.c
+++ C/src/TEOBResumSUtils.c
@@ -242,11 +242,12 @@ void compute_hpc(Waveform_lm *hlm, double nu, double M, double distance, double 
   double Aki, cosPhi, sinPhi;
   double sumr, sumi;
   int activemode[KMAX];
+  double Msun = M;
   set_multipolar_idx_mask(activemode, KMAX, "use_mode_lm", 1);
-  if (!(par_get_i("use_geometric_units"))) M = M/MSUN_S;
+  if (!(par_get_i("use_geometric_units"))) Msun = M/MSUN_S;
 #if (DEBUG)
     printf("h+,x: nu = %e M = %e D = %e Mpc psi = %e iota = %e prefactor = %e\n",
-	   nu,M,distance,psi,iota,amplitude_prefactor);
+	   nu,Msun,distance,psi,iota,amplitude_prefactor);
 #endif
   for (int i = 0; i < hlm->size; i++) {
     hpc->time[i] = hlm->time[i]*M; 
