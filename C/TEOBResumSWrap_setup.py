5ac74d28187ecdcb32a482692c697902540138b9
Rossella <rossella.gamba@ligo.org>
2019-12-19 21:49:35 +0100

    minor cleanup of setup.py

--- C/TEOBResumSWrap_setup.py
+++ C/TEOBResumSWrap_setup.py
@@ -1,7 +1,9 @@
 """
-Setup script for CPython_test.c
+Setup script for TEOBResumSWrap.c
 Compile with 
-$ python CPython_test_setup.py build_ext --inplace
+$ python TEOBResumSWrap_setup.py build_ext --inplace
+
+Inspired by https://github.com/ICRAR/pyprofit/issues/4
 """
 
 from distutils.core import setup, Extension
@@ -10,13 +12,6 @@ import numpy
 import glob
 #setup(ext_modules=[Extension("EOBRunTD_module", ["TEOBResumSWrap.c", "TEOBResumSDynamics.c", "TEOBResumSEOBRun.c", "TEOBResumSFits.c", "TEOBResumSFlux.c", "TEOBResumSInitialConditions.c", "TEOBResumSMetric.c", "TEOBResumSOMP.c", "TEOBResumSPars.c", "TEOBResumSPostAdiab.c", "TEOBResumSUtils.c", "TEOBResumSWaveform.c", "TEOBResumS.c"], include_dirs=[numpy.get_include()]),],)
 
-#def has_gsl():
-#    compiler = distutils.ccompiler.new_compiler()
-#    compiler.add_include_dir('/usr/include') #RAPH
-#    compiler.add_library_dir('/usr/local/lib') #RAPH
-#    return compiler.has_function('gsl_sf_gamma', libraries=['gsl', 'gslcblas'])
-#    return compiler.has_function('gsl_sf_gamma', libraries=['/usr/local/lib/gsl', '/usr/local/lib/gslcblas'])
-
 # Our module
 eob_sources = ['TEOBResumSWrap.c']
 
@@ -24,16 +19,8 @@ eob_sources = ['TEOBResumSWrap.c']
 eob_sources += glob.glob('src/*.c')
 
 # include dirs
-#incdirs = ['libprofit', '/usr/local/include'] #RAPH
-#incdirs = ['libprofit', '/usr/local/include']
 incdirs = [numpy.get_include(), '/usr/include/']
 
-# gsl libs
-#if not has_gsl():
-#    print("\n\nNo GSL installation found on your system. Install the GSL development package and try again\n\n")
-#    sys.exit(1)
-
-#libs = ['gsl', 'gslcblas']
 libs = ['gsl', 'gslcblas', 'm', 'config']
 
 pyprofit_ext = Extension('EOBRunTD_module',
@@ -42,8 +29,8 @@ pyprofit_ext = Extension('EOBRunTD_module',
                        define_macros = [('HAVE_GSL',1)],
                        sources = eob_sources,
                        include_dirs = incdirs,
-                       libraries = libs
-                       #extra_compile_args=["-lgsl -lgslcblas -lm -lconfig"]
+                       libraries = libs,
+                       extra_compile_args=["-w"] #to ignore all warnings (do not use when debugging)
                        )
 
 setup(ext_modules=[pyprofit_ext],)
