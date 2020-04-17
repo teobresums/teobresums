ea26e0e4e75e6aa5a41a3a42d8a57d68fc91cbe2
Rossella <rossella.gamba@ligo.org>
2020-04-17 13:04:52 +0200

    add eccentricity and hyp parameters to example

--- C/run_test.py
+++ C/run_test.py
@@ -3,6 +3,7 @@ import EOBRun_module
 import matplotlib.pyplot as plt
 
 pars = {
+<<<<<<< HEAD
     'M': 50.,
     'q': 1.,
     'chi1': 0.,
@@ -16,6 +17,31 @@ pars = {
     }
 
 t, hp, hc = EOBRun_module.EOBRunTD(pars)
+=======
+    'M'                  : 50.,
+    'q'                  : 1.,
+    'chi1'               : 0.,
+    'chi2'               : 0.,
+    'Lambda1'            : 0.,
+    'Lambda2'            : 0.,     
+    'domain'             : 0,      #Set 1 for FD. Default = 0
+    'arg_out'            : 1,      #Output hlm/hflm. Default = 0
+    'use_mode_lm'        : k,      #List of modes to use/output through EOBRunPy
+    'output_lm'          : k,      #List of modes to print on file
+    'srate_interp'       : 4096.,  #srate at which to interpolate. Default = 4096.
+    'use_geometric_units': 0,      #output quantities in geometric units. Default = 1
+    'df'                 : 0.01,   #df for FD interpolation
+    'initial_frequency'  : 20.,    #in Hz if use_geometric_units = 0, else in geometric units
+    'interp_uniform_grid': 2,      #interpolate mode by mode on a uniform grid. Default = 0 (no interpolation)
+    'ecc'                : 0.1,    #Eccentricity. Default = 0.
+    'j_hyp'              : 0.,     #J_hyp. Default = 0.
+    'r_hyp'              : 0.,     #r_hyp. Default = 0.
+    'H_hyp'              : 0.,     #H_hyp. Default = 0.
+}
+
+#Run the WF generator
+t, hp, hcm, hlm = EOBRun_module.EOBRunPy(pars)
+>>>>>>> b2cc8b0... add eccentricity and hyp parameters to example
 
 plt.plot(t, hp)
 plt.show()
