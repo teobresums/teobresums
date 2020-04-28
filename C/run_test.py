9a0bb33a9206b8d353f124abf94d9b917edaed17
Rossella Gamba <rossella.gamba@uni-jena.de>
2020-04-28 13:40:03 +0000

    fix run_test.py

--- C/run_test.py
+++ C/run_test.py
@@ -34,9 +34,6 @@ t, hp, hc = EOBRun_module.EOBRunTD(pars)
     'initial_frequency'  : 20.,    #in Hz if use_geometric_units = 0, else in geometric units
     'interp_uniform_grid': 2,      #interpolate mode by mode on a uniform grid. Default = 0 (no interpolation)
     'ecc'                : 0.1,    #Eccentricity. Default = 0.
-    'j_hyp'              : 0.,     #J_hyp. Default = 0.
-    'r_hyp'              : 0.,     #r_hyp. Default = 0.
-    'H_hyp'              : 0.,     #H_hyp. Default = 0.
 }
 
 #Run the WF generator
