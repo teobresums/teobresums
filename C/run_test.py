851c952d9d9fdd6f2714530204833ca8cce00b6c
Piero Rettegno <piero.rettegno@ligo.org>
2020-07-13 12:36:54 +0200

    Removed double functions

--- C/run_test.py
+++ C/run_test.py
@@ -3,21 +3,6 @@ import EOBRun_module
 import matplotlib.pyplot as plt
 
 pars = {
-<<<<<<< HEAD
-    'M': 50.,
-    'q': 1.,
-    'chi1': 0.,
-    'chi2': 0.,
-    'Lambda1': 0.,
-    'Lambda2': 0.,
-    'interp_uniform_grid':0,
-    'use_geometric_units':0,
-    'initial_frequency':20,
-    'ecc':0.1
-    }
-
-t, hp, hc = EOBRun_module.EOBRunTD(pars)
-=======
     'M'                  : 50.,
     'q'                  : 1.,
     'chi1'               : 0.,
@@ -38,7 +23,6 @@ t, hp, hc = EOBRun_module.EOBRunTD(pars)
 
 #Run the WF generator
 t, hp, hcm, hlm = EOBRun_module.EOBRunPy(pars)
->>>>>>> b2cc8b0... add eccentricity and hyp parameters to example
 
 plt.plot(t, hp)
 plt.show()
