2bb68b714ed7d7e3ad3e0f8563ecd98edb2d41cd
sebastiano bernuzzi <sebastiano.bernuzzi@gmail.com>
2020-08-03 21:00:10 +0200

    added a workaround for the hyp initial conditions: the code now does not raise an error, but the routine returns a failure code, then the main catch it and terminaes without exception by returning a zero dynamics and wavefom. This is hack, initial conditions needs to be properly implemented in such a way the code does not fail or does something safe. Modifs to the hack.

--- C/src/TEOBResumS.c
+++ C/src/TEOBResumS.c
@@ -1184,7 +1184,10 @@ int EOBRun(Waveform **hpc, WaveformFD **hfpc,
     if (r_hyp != 0.) {
       //SB 08/2020 workaround 
       //FIXME!!!!
-      if (eob_dyn_ic_hyp(r_hyp, H_hyp, j_hyp, dyn, dyn->y0)) return OK;
+      if (eob_dyn_ic_hyp(r_hyp, H_hyp, j_hyp, dyn, dyn->y0)) {
+	status = 1;
+	goto EXIT_POINT;
+      }
     } else if (ecc != 0.) {
       eob_dyn_ic_ecc(r0, dyn, dyn->y0);
     } else if (use_spins) {
