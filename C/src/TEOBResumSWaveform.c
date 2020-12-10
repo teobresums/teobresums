a060f275c6cd5a06139472896b62a9c70f79a927
Rossella <rossella.gamba@ligo.org>
2020-12-10 16:32:25 +0100

    small wigner d, exchange emm, n indices (notation)

--- C/src/TEOBResumSWaveform.c
+++ C/src/TEOBResumSWaveform.c
@@ -4205,7 +4205,7 @@ void twist_hlm_TD(Dynamics *dyn, Waveform_lm *hlm, DynamicsSpin *spin, int inter
     
           // d^l_{m,s}(angle)
           //CHECKME: index correct of Wigner matrices correct?
-          double dl_mn = wigner_d_function(ell, emm,n, -beta[i]);
+          double dl_mn = wigner_d_function(ell, n,emm, -beta[i]);
 
           // hlm modes are given as phase/amplitude
           // but here we need real/imag
