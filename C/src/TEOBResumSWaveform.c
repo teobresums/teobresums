c41e97b8abbd1a21e6824136e60fc10a7a500a87
Rossella <rossella.gamba@ligo.org>
2021-01-19 16:29:11 +0100

    go back from ecc> 1e-4 to ecc !=0 in a number of places. We run teobresums_circ if ecc=0, teobresums_ecc with circular IC if ecc < 1e-4 and teobresums_ecc with eccentric IC if ecc>1e-4

--- C/src/TEOBResumSWaveform.c
+++ C/src/TEOBResumSWaveform.c
@@ -2371,7 +2371,7 @@ void eob_wav_hlmNQC_find_a1a2a3(Dynamics *dyn, Waveform_lm *h, Waveform_lm *hnqc
   }
 
   
-  if ((ecc > 1e-4) || (r_hyp != 0.)) {
+  if ((ecc != 0.) || (r_hyp != 0.)) {
     double t0 = tNQC - 30.;
     double alpha = 0.09;
     double *smooth_theta;
@@ -2802,7 +2802,7 @@ void eob_wav_hlmNQC_find_a1a2a3_mrg_HM(Dynamics *dyn_mrg, Waveform_lm *hlm_mrg, 
     PRFORMd("b2",bi[1][1]);
   }
 
-  if ((ecc > 1e-4) || (r_hyp != 0.)) {
+  if ((ecc != 0.) || (r_hyp != 0.)) {
     double t0 = tNQC - 30.;
     double alpha = 0.09;
     double *smooth_theta;
@@ -2900,7 +2900,7 @@ void eob_wav_hlmNQC_find_a1a2a3_mrg_HM(Dynamics *dyn_mrg, Waveform_lm *hlm_mrg, 
     }
   }
 
-  if ((ecc > 1e-4) || (r_hyp != 0.)) {
+  if ((ecc != 0.) || (r_hyp != 0.)) {
     double t0 = tNQC - 30.;
     double alpha = 0.09;
     double *smooth_theta;
@@ -3240,7 +3240,7 @@ void eob_wav_hlmNQC_find_a1a2a3_mrg_22(Dynamics *dyn_mrg, Waveform_lm *hlm_mrg, 
     PRFORMd("b2",bi[k22][1]);
   }
 
-  if ((ecc > 1e-4) || (r_hyp != 0.)) {
+  if ((ecc != 0.) || (r_hyp != 0.)) {
     double t0 = tNQC - 30.;
     double alpha = 0.09;
     double *smooth_theta;
@@ -3300,7 +3300,7 @@ void eob_wav_hlmNQC_find_a1a2a3_mrg_22(Dynamics *dyn_mrg, Waveform_lm *hlm_mrg, 
     n5[k22][j]  = n4[k22][j]*r2*w2;              /* (pr*)*(r Omg) */
   }
 
-  if ((ecc > 1e-4) || (r_hyp != 0.)) {
+  if ((ecc != 0.) || (r_hyp != 0.)) {
     double t0 = tNQC - 30.;
     double alpha = 0.09;
     double *smooth_theta;
