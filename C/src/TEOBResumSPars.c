bf818a79a245d20058ec17d9859cec0d96c5c24d
Rossella <rossella.gamba@ligo.org>
2021-01-19 16:29:11 +0100

    go back from ecc> 1e-4 to ecc !=0 in a number of places. We run teobresums_circ if ecc=0, teobresums_ecc with circular IC if ecc < 1e-4 and teobresums_ecc with eccentric IC if ecc>1e-4

--- C/src/TEOBResumSPars.c
+++ C/src/TEOBResumSPars.c
@@ -1239,7 +1239,7 @@ void eob_set_params(int default_choice, int firstcall)
       EOBPars->nqc_coefs_flx = NQC_FLX_NONE;
       EOBPars->nqc_coefs_hlm = NQC_HLM_NONE;
     } else {
-      if ((ecc > 1e-4) || (r_hyp != 0.)) {
+      if ((ecc != 0.) || (r_hyp != 0.)) {
 	EOBPars->nqc_coefs_flx = NQC_FLX_NONE;
 	EOBPars->nqc_coefs_hlm = NQC_HLM_COMPUTE;
       } else if (usespins) {
@@ -1254,7 +1254,7 @@ void eob_set_params(int default_choice, int firstcall)
   
   /** Set more as needed ... */
   EOBPars->a6c = 0.;
-  if (ecc > 1e-4) {
+  if (ecc != 0.) {
     /* Eccentric case */
     EOBPars->a6c = eob_a6c_fit_HM(EOBPars->nu); // For the moment, use this still
     //EOBPars->a6c = eob_a6c_fit_ecc(EOBPars->nu);
@@ -1340,7 +1340,7 @@ void eob_set_params(int default_choice, int firstcall)
   } else errorexit("unknown option for use_flm");
 
   /** Set hlm fun pointer */
-  if ((ecc > 1e-4) || (r_hyp != 0.)) {
+  if ((ecc != 0.) || (r_hyp != 0.)) {
     eob_wav_hlm = &eob_wav_hlm_ecc;
   } else  {
     eob_wav_hlm = &eob_wav_hlm_circ;
@@ -1607,7 +1607,7 @@ void eob_set_params_old(char *s, int n)
   
   /** Set more as needed ... */
   double a6c = 0.;
-  if (EOBPars->ecc > 1e-4) {
+  if (EOBPars->ecc != 0.) {
     /* Eccentric case */
     EOBPars->a6c = eob_a6c_fit_ecc(EOBPars->nu);
   } else if (EOBPars->use_flm == USEFLM_HM) {
