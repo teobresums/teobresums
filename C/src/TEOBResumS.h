58846e57a12ebd3f3ef993e23c76473dd4cc3b43
Alejandra Gonzalez <alejandra.gonzalez@uni-jena.de>
2021-04-27 16:14:06 +0200

    Added function for spinning BHNS tidal disrupted QNM fits

--- C/src/TEOBResumS.h
+++ C/src/TEOBResumS.h
@@ -649,7 +649,8 @@ void errorexits(char *file, int line, const char *s, const char *t);
 #define errorexits(s,t) errorexits(__FILE__, __LINE__, (s), (t))
 
 /* TEOBResumSFits.c */
-void tidal_disruption_cases(double q, double Mf, double M, bool *flag, bool *flag2);
+void tidal_disruption_cases(double q, double Mf, double M, double chi1, bool *flag, bool *flag2, bool *flag3);
+void QNM_bhns_td(double af, double *alpha1, double *alpha2, double *omega1, double kapT2, double nu);
 void kerr_bh_freq_td(double *omega1, double *omega2, double k2t);
 void kerr_bh_freq(double *omega1, double *omega2, double a_bh, double Mbh, double k2t, bool td2);
 void kerr_bh_qnm_td(double *alpha1, double *alpha2, double k2t);
