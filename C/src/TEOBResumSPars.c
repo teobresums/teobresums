teobresums/teobresums

commit d0b044b85aa8fc81553ef7da74d4201fedecf030

Added function to update function pointers, plus some minor corrections

C/src/TEOBResumSPars.c
@@ -1323,3 +1323,23 @@ int main (int argc, char* argv[])
 }
 
 #endif
+
+void update_params(int binary)
+{
+  /* Updated function pointers */
+  
+  /** Set f_lm fun pointer */
+  if (EOBPars->use_flm == USEFLM_HM) {
+    eob_wav_hlmNewt  = &eob_wav_hlmNewt_HM;
+    eob_wav_flm      = &eob_wav_flm_HM;
+    eob_wav_flm_s    = &eob_wav_flm_s_HM;
+    eob_wav_deltalm  = &eob_wav_deltalm_HM;
+    if(binary==BINARY_BBH){
+      eob_wav_hlmNQC_find_a1a2a3_mrg = &eob_wav_hlmNQC_find_a1a2a3_mrg_HM;
+      eob_wav_ringdown = &eob_wav_ringdown_HM; 
+    }else{
+      eob_wav_hlmNQC_find_a1a2a3_mrg = &eob_wav_hlmNQC_find_a1a2a3_mrg_BHNS_HM;
+      eob_wav_ringdown = &eob_wav_ringdown_bhns;
+    }
+  } else errorexit("unknown option for use_flm");
+}

C/src/TEOBResumS.c
@@ -357,7 +357,7 @@ int EOBRun(Waveform **hpc, WaveformFD **hfpc,
     /* Set ODE stop to LR */
     EOBPars->ode_stop_radius = 1.01*EOBPars->rLR_tidal;    
   }
-  if (EOBPars->compute_LR) {
+  if (EOBPars->compute_LR && !(use_tidal)) {
     //TODO: LR COMPUTATION IS CORRECT ONLY FOR NOSPIN. IMPLEMENT SPIN VERSION IN eob_dyn_adiabLSO()
     ROOTFINDER(check_status, eob_dyn_adiabLR(dyn, &(EOBPars->rLR)));
     if (check_status) {
@@ -381,6 +381,9 @@ int EOBRun(Waveform **hpc, WaveformFD **hfpc,
   /** Compute the dressing factors for the f-mode resonances at r0 */
   if ((EOBPars->use_tidal)&&(EOBPars->use_tidal_fmode_model))
     fmode_resonance_dressing_factors(r0, dyn);  
+
+  /** Update function pointers if necessary for BHNS**/
+  update_params(EOBPars->binary);
 
   /* Iteration index */
   int iter   = 0;  
@@ -941,12 +944,8 @@ int EOBRun(Waveform **hpc, WaveformFD **hfpc,
 	      add to both merger and full waveform */
         Waveform_lm_alloc (&hlm_nqc, hlm_mrg->size, "hlm_nqc"); 
         /* eob_wav_hlmNQC_find_a1a2a3_mrg_22(dyn_mrg, hlm_mrg, hlm_nqc, dyn, hlm); */
-        
-        if(EOBPars->binary==BINARY_BBH){ 
-          eob_wav_hlmNQC_find_a1a2a3_mrg(dyn_mrg, hlm_mrg, hlm_nqc, dyn, hlm);
-        }else{
-          eob_wav_hlmNQC_find_a1a2a3_mrg_BHNS_HM(dyn_mrg, hlm_mrg, hlm_nqc, dyn, hlm);
-        }
+  
+        eob_wav_hlmNQC_find_a1a2a3_mrg(dyn_mrg, hlm_mrg, hlm_nqc, dyn, hlm);
         
 
         strcat(hlm_mrg->name,"_nqc");
@@ -1022,11 +1021,7 @@ int EOBRun(Waveform **hpc, WaveformFD **hfpc,
     EOBPars->size = size;
     
     /* Ringdown attachment */
-    if(EOBPars->binary==BINARY_BBH){
       eob_wav_ringdown(dyn, hlm);
-    }else{
-      eob_wav_ringdown_bhns(dyn, hlm);
-    }
     
   } /* End of BBH section */
 

C/src/TEOBResumS.h
@@ -674,6 +674,7 @@ void EOBParameters_parse_file(char *fname, EOBParameters *eobp);
 int EOBParameters_parse_commandline(EOBParameters *eobp, int argc, char **argv);
 void EOBParameters_set_key_val(EOBParameters *eobp, char *key, char *val);
 void EOBParameters_tofile(EOBParameters *eobp,char *fname);
+void update_params(int binary);
 
 /* TEOBResumSUtil.c */
 double q_to_nu(const double q);

C/src/TEOBResumSWaveform.c
@@ -5944,6 +5944,7 @@ void compute_hpc_FD(WaveformFD_lm *hflm, double nu, double M, double distance, d
 /** BHNS NQC **/
 
 // NQC model with double derivatives for BHNS, TEST
+// may be needed later when more NR data are available
 void eob_wav_hlmNQC_test_bhns(Dynamics *dyn_mrg, Waveform_lm *hlm_mrg, Waveform_lm *hnqc,
 				       Dynamics *dyn, Waveform_lm *hlm, bool *bhns)
 {

commit 9c59b246af1db5b90a320b2d5222701b78359868

fig bug for BNS introduced in d0b044b (see issue 12)

C/src/TEOBResumSPars.c
@@ -645,6 +645,26 @@ void eob_set_params(int default_choice, int firstcall)
 
 }
 
+void update_params(int binary)
+{
+  /* Updated function pointers */
+  
+  /** Set f_lm fun pointer */
+  if (EOBPars->use_flm == USEFLM_HM) {
+    eob_wav_hlmNewt  = &eob_wav_hlmNewt_HM;
+    eob_wav_flm      = &eob_wav_flm_HM;
+    eob_wav_flm_s    = &eob_wav_flm_s_HM;
+    eob_wav_deltalm  = &eob_wav_deltalm_HM;
+    if(binary==BINARY_BBH){
+      eob_wav_hlmNQC_find_a1a2a3_mrg = &eob_wav_hlmNQC_find_a1a2a3_mrg_HM;
+      eob_wav_ringdown = &eob_wav_ringdown_HM; 
+    }else if(binary==BINARY_BHNS){
+      eob_wav_hlmNQC_find_a1a2a3_mrg = &eob_wav_hlmNQC_find_a1a2a3_mrg_BHNS_HM;
+      eob_wav_ringdown = &eob_wav_ringdown_bhns;
+    }
+  }
+}
+
 int EOBParameters_parse_commandline(EOBParameters *eobp, int argc, char **argv)
 {
   optind=1; //in order to parse twice, this needs to be 1
@@ -1323,23 +1343,3 @@ int main (int argc, char* argv[])
 }
 
 #endif
-
-void update_params(int binary)
-{
-  /* Updated function pointers */
-  
-  /** Set f_lm fun pointer */
-  if (EOBPars->use_flm == USEFLM_HM) {
-    eob_wav_hlmNewt  = &eob_wav_hlmNewt_HM;
-    eob_wav_flm      = &eob_wav_flm_HM;
-    eob_wav_flm_s    = &eob_wav_flm_s_HM;
-    eob_wav_deltalm  = &eob_wav_deltalm_HM;
-    if(binary==BINARY_BBH){
-      eob_wav_hlmNQC_find_a1a2a3_mrg = &eob_wav_hlmNQC_find_a1a2a3_mrg_HM;
-      eob_wav_ringdown = &eob_wav_ringdown_HM; 
-    }else{
-      eob_wav_hlmNQC_find_a1a2a3_mrg = &eob_wav_hlmNQC_find_a1a2a3_mrg_BHNS_HM;
-      eob_wav_ringdown = &eob_wav_ringdown_bhns;
-    }
-  } else errorexit("unknown option for use_flm");
-}