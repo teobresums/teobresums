9c0e8b6e9951f338c20713f74812f278c1f1c25b
Rossella <rossella.gamba@ligo.org>
2024-01-12 05:53:22 -0500

    Add pph_lso_orbital declaration

--- C/src/TEOBResumS.h
+++ C/src/TEOBResumS.h
@@ -970,6 +970,7 @@ void eob_nqc_point_test(double Mbh, double c1A, double c2A, double c3A, double c
 			    double alpha1, double omega1,
 			    double *A_tmp, double *dA_tmp, double *d2A_tmp, double *omg_tmp, double *domg_tmp, double *d2omg_tmp);
 double horizon_radius(const double nu);
+double pph_lso_orbital(const double nu);
 
 /* TEOBResumSDynamics.c */
 extern int (*p_eob_dyn_rhs)(); /* defined in TEOBResumSPars.c */
