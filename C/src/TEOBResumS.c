163d1d949fbcffc008bd9880d6cf9e0f4034735d
lcelandic <danilochiaramello@gmail.com>
2024-11-04 16:45:33 +0100

    Fix: deleted extra print statements in C main.

--- C/src/TEOBResumS.c
+++ C/src/TEOBResumS.c
@@ -1247,9 +1247,7 @@ int EOBRun(Waveform **hpc, WaveformFD **hfpc,
       /* Interp to uniform grid the multipoles before hpc computation */
       const double dt_interp = EOBPars->dt_interp;
       const long int size_interp = get_uniform_size(hlm->time[size-1], hlm->time[0], dt_interp); 
-      printf("Interpolating to uniform grid with dt = %e\n",dt_interp);
       Waveform_lm_interp (hlm, size_interp, hlm->time[0], dt_interp, "hlm_interp");
-      printf("Interpolateds\n");
       size = size_interp;
     }
 
