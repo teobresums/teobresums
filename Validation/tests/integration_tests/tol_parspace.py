c02c5acfd579875225c7360450f46c771e80b8ae
lcelandic <danilochiaramello@gmail.com>
2025-05-30 17:07:55 +0200

    Fix: align wfs at start when comparing different tolerances. Misc updates.

--- Validation/tests/integration_tests/tol_parspace.py
+++ Validation/tests/integration_tests/tol_parspace.py
@@ -17,7 +17,7 @@ try:
     from PyART.utils import wf_utils as wfu
     pyart = True
 except:
-    print("Warning! PyART not installed; not computing mismatch or phase difference.")
+    print("Warning! PyART not installed; not computing mismatch.")
     pyart = False
 
 MSUN = 4.925490947641267e-06
@@ -70,6 +70,9 @@ def run_tols(delta_log_tol, pardic, f0):
         clock0 = time.time() - clock0
         t0 = t0 - t0[0]
 
+        # Merger index on default wf
+        jmrg = np.argmax(hlm0['1'][0])
+
         # Build waveform object by hand for match
         if pyart:
             eob0 = wf.Waveform()
@@ -77,8 +80,6 @@ def run_tols(delta_log_tol, pardic, f0):
             eob0._hlm    = teob.convert_hlm(hlm0)
             eob0._kind   = 'EOB'
             eob0.domain  = 'Time'
-            # Alignment time interval
-            t_align = np.linspace(eob0.u[0] + 0.1*(eob0.u[-1] - eob0.u[0]), eob0.u[0] + 0.75*(eob0.u[-1] - eob0.u[0]), 10000)
         
         change_tols = [{'ode_abstol': 10**(-13 + delta_log_tol), 'ode_reltol': 10**(-11)}, 
                        {'ode_abstol': 10**(-13),                 'ode_reltol': 10**(-11 + delta_log_tol)}, 
@@ -93,6 +94,9 @@ def run_tols(delta_log_tol, pardic, f0):
             times.append((time.time() - clock)/clock0)
             t = t - t[0]
 
+            # Get phase difference
+            dphis.append(np.interp(t0[jmrg], t, np.unwrap(hlm['1'][1])) - np.unwrap(hlm0['1'][1])[jmrg])
+
             # Build waveform object by hand for match
             if pyart:
                 eob = wf.Waveform()
@@ -101,30 +105,18 @@ def run_tols(delta_log_tol, pardic, f0):
                 eob._kind  = 'EOB'
                 eob.domain = 'Time'
 
-                # Align with base case
-                tau, dphi, _ = wfu.Align(t_align, t_align[-1], 0.1*t_align[-1], eob.u, eob.hlm[(2,2)]['p'], eob0.u, eob0.hlm[(2,2)]['p'])
-                eob._u = eob.u + tau
-                eob._hlm[(2,2)]['p'] = eob.hlm[(2,2)]['p'] - dphi
-                eob._hlm[(2,2)]['z'] = eob.hlm[(2,2)]['z'] * np.exp(-1j*dphi)
-                eob._hlm[(2,2)]['real'] = eob.hlm[(2,2)]['z'].real
-                eob._hlm[(2,2)]['imag'] = eob.hlm[(2,2)]['z'].imag
-
-                # Get phase difference
-                dphis.append((np.interp(eob0.u, eob.u, eob.hlm[(2,2)]['p']) - eob0.hlm[(2,2)]['p'])[np.argmax(eob0.hlm[(2,2)]['A'])])
-
                 # Compute mismatch
                 mismatch = match.Matcher(eob0, eob, settings={**match_settings, **{'M': newdic['M'], 'initial_frequency_mm': 1.35*newdic['initial_frequency']}})
                 matches.append(mismatch.mismatch)
             else:
-                dphis   = [1, 1, 1]
                 matches = [1, 1, 1]
     except:
         times   = [1, 1, 1]
         dphis   = [1, 1, 1]
         matches = [1, 1, 1]
 
-    return {**pardic, **{'delta_log_tol': delta_log_tol, 'time_abs': times[0],  'dphi_abs': dphis[0],  'match_abs': matches[0],
-                                                         'time_rel': times[1],  'dphi_rel': dphis[1],  'match_rel': matches[1],
+    return {**pardic, **{'delta_log_tol': delta_log_tol, 'time_abs' : times[0],  'dphi_abs': dphis[0],  'match_abs': matches[0],
+                                                         'time_rel' : times[1],  'dphi_rel': dphis[1],  'match_rel': matches[1],
                                                          'time_both': times[2], 'dphi_both': dphis[2], 'match_both': matches[2]}}
 
 def dict_to_vec(dic):
@@ -145,20 +137,20 @@ if __name__=='__main__':
     parser.add_argument('--N',          type=int,            default=10000,          help='Number of random parameter sets to generate.')
     parser.add_argument('--M',          type=float, nargs=2, default=[10., 200.],    help='Mass range (in solar masses) for the binary.')
     parser.add_argument('--q',          type=float, nargs=2, default=[1., 10.],      help='Mass ratio range for the binary.')
-    parser.add_argument('--f0',         type=float,          default=10.,            help='Initial frequency.')
-    parser.add_argument('--chi',        type=float, nargs=2, default=[-0.99, 0.99],  help='Spin range for the binary.')
+    parser.add_argument('--f0',         type=float,          default=10.,            help='Initial frequency [Hz].')
+    parser.add_argument('--chi',        type=float, nargs=2, default=[-0.99, 0.99],  help='Spin range for the binary (spin aligned, z-component).')
     parser.add_argument('--eccentric',  action='store_true',                         help='Eccentric?')
     parser.add_argument('--ecc',        type=float, nargs=2, default=[0., 0.6],      help='Eccentricity range for the binary.')
     parser.add_argument('--anomaly',    type=float, nargs=2, default=[0., 2.*np.pi], help='Anomaly range for the binary.')
     parser.add_argument('--precessing', action='store_true',                         help='Precessing?')
-    parser.add_argument('--dlogtol',    type=int,            default=4,              help='Log tolerance increase w.r.t. default.')
+    parser.add_argument('--dlogtol',    type=int,            default=2,              help='Log tolerance increase w.r.t. default.')
     parser.add_argument('--nprocs',     type=int,            default=8,              help='Number of processes if running parallel.')
     parser.add_argument('--plot',       action='store_true',                         help='Plot results?')
     parser.add_argument('--file',       type=str,            default=None,           help='Input file')
     parser.add_argument('--px',         type=str,            default='q',            help='x variable on plots')
     parser.add_argument('--py',         type=str,            default='chieff',       help='y variable on plots')
     parser.add_argument('--pc',         type=str,            default='match',        help='Color variable on plots')
-    parser.add_argument('--worst',      action='store_true',                         help='Plot the worst matching wfs according to the given pc variable')
+    parser.add_argument('--worst',      action='store_true',                         help='Plot the worst matching wf according to the given pc variable')
 
     args = parser.parse_args()
 
@@ -259,14 +251,11 @@ if __name__=='__main__':
             t = t - t[0]
 
             if pyart:
-                t_align = np.linspace(t0[0] + 0.1*(t0[-1] - t0[0]), t0[0] + 0.75*(t0[-1] - t0[0]), 10000)
-                tau, dphi, _ = wfu.Align(t_align, t_align[-1], 0.1*t_align[-1], t, hlm['1'][1], t0, hlm0['1'][1])
-                t = t + tau
-                deltaphi = np.interp(t0, t, hlm['1'][1]) - dphi - hlm0['1'][1]
+                deltaphi = np.interp(t0, t, np.unwrap(hlm['1'][1])) - np.unwrap(hlm0['1'][1])
 
                 fig, ax = plt.subplots(2, 1, layout='constrained', figsize=(9,8), sharex=True)
                 ax[0].plot(t0, hlm0['1'][0]*np.cos(hlm0['1'][1]), color='k')
-                ax[0].plot(t,  hlm['1'][0]*np.cos(hlm['1'][1] - dphi))
+                ax[0].plot(t,  hlm['1'][0]*np.cos(hlm['1'][1]))
                 ax[1].plot(t0, deltaphi)
                 ax[0].set_ylabel(r'$\Re h_{22}$')
                 ax[1].set_xlabel(r'$t/M$')
