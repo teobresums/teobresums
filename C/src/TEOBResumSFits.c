91d4cef5c4f3ac3d79a7ad85bfc3d473684357e1
danilochiaramello <danilochiaramello@gmail.com>
2026-06-04 19:58:11 +0200

    Fix: bugs in second derivatives of amplitude and frequency of ringdown template.

--- C/src/TEOBResumSFits.c
+++ C/src/TEOBResumSFits.c
@@ -1145,7 +1145,7 @@ void eob_nqc_point_postpeak_d2(double Mbh, double c1A, double c2A, double c3A, d
   dA_tmp1     = c1A*c2A*expt/(fact*fact);
   dA_tmp2     = -alpha1*expt*(c4A + c1A*tanh(c3A+ c2A*tau));
   *dA_tmp     = (dA_tmp1 + dA_tmp2)/Mbh;
-  *d2A_tmp    = (alpha1*alpha1*(expt*(c1A*tanht + c4A)) - 2.0*expt*c1A*c2A*(alpha1 - c2A*tanht)/(fact*fact))/Mbh2;
+  *d2A_tmp    = (alpha1*alpha1*(expt*(c1A*tanht + c4A)) - 2.0*expt*c1A*c2A*(alpha1 + c2A*tanht)/(fact*fact))/Mbh2;
   
   x        = exp(-c2phi*tau);
   x2       = x*x;
@@ -1158,7 +1158,7 @@ void eob_nqc_point_postpeak_d2(double Mbh, double c1A, double c2A, double c3A, d
   domg_tmp_n2 = c1phi*(4.*SQ(c2phi)*c4phi*x2 + SQ(c2phi)*c3phi*x);
   domg_tmp_d2 = 1 + c4phi*x2 + c3phi*x;
   *domg_tmp   = -(domg_tmp_n1/domg_tmp_d1 - domg_tmp_n2/domg_tmp_d2)/Mbh2;
-  *d2omg_tmp  = c1phi*c2phi*c2phi*(c3phi + 4.0*c4phi*x + c3phi*c4phi*x2)/(domg_tmp_d1)/Mbh2/Mbh;
+  *d2omg_tmp  = -c1phi*c2phi*c2phi*c2phi*x*(1.0 - c4phi*x2)*(8.0*c4phi*x*(1.0 + c4phi*x2) + c3phi*(1.0 + x2*(10.0*c4phi - c3phi*c3phi + c4phi*c4phi*x2)))/domg_tmp_d1/domg_tmp_d2/Mbh2/Mbh;
 }
 /** @} */ // end of nqcpoint
 
